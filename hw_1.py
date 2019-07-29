1570545e02700d09ee1e7a4f57e21aa02013d6f1
OKhoma04 <khomaolga04@gmail.com>
2019-07-29 15:25:52 +0300

    Revert "Update hw_1.py"
    This reverts commit 590cb4593ec0db146bb41f14e5a9b57e4c16677e.

--- hw_1.py
+++ hw_1.py
@@ -1,36 +1,19 @@
-import re
 # create class, clean punctuation and rewrite text file
+import re
 class TextCleaner:
     uncleanText = open('C:\Book.txt').read()
     cleanText = re.sub('[^A-Za-z0-9\s]+', '', uncleanText)
     open('C:\Book.txt', 'w').write(cleanText)
-# create function, replace values due to dictionary
-def replace_words(base_text, device_values):
-    for key, val in device_values.items():
-        base_text = base_text.replace(key, val)
-    return base_text
 
-# create dictionary, replace roman numerals to int
-device = {"ACT I":"ACT 1", "ACT II":"ACT 2", "ACT III":"ACT 3", "ACT IV":"ACT 4", "ACT V":"ACT 5"}
+text=open('C:\Book.txt').read()
+text = text.lower()
 
-# Open desired file as 't' and read the lines into string 'tempstr'
-t = open('C:\Book.txt', 'r')
-tempstr = t.read()
-t.close()
-
-# replacement and lower case
-output = replace_words(tempstr, device)
-tempstr = tempstr.lower()
-
-# write out the new file
-fout = open('output.txt', 'w')
-fout.write(output)
-fout.close()
 
 # create list of words
-word_list=tempstr.split()
+word_list=text.split()
 print(word_list)
 
+#text.close()
 #create dictionary
 d = {}
 
@@ -40,3 +23,5 @@ for word in word_list:
 #count words
 for key,val in d.items():
     print (key, val, 'times')
+
+
