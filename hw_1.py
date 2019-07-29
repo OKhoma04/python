fbbeaac8a47938654e18d28b4810a73184922186
OKhoma04 <khomaolga04@gmail.com>
2019-07-29 11:31:58 +0300

    Update hw_1.py
    add text = text.lower()

--- hw_1.py
+++ hw_1.py
@@ -1,16 +1,19 @@
 # create class, clean punctuation and rewrite text file
 import re
 class TextCleaner:
-    uncleanText = open('C:\Book[9250].txt').read()
+    uncleanText = open('C:\Book.txt').read()
     cleanText = re.sub('[^A-Za-z0-9\s]+', '', uncleanText)
-    open('C:\Book[9250].txt', 'w').write(cleanText)
+    open('C:\Book.txt', 'w').write(cleanText)
 
-text=open('C:\Book[9250].txt').read()
+text=open('C:\Book.txt').read()
+text = text.lower()
+
 
 # create list of words
 word_list=text.split()
 print(word_list)
 
+#text.close()
 #create dictionary
 d = {}
 
@@ -20,3 +23,5 @@ for word in word_list:
 #count words
 for key,val in d.items():
     print (key, val, 'times')
+
+
