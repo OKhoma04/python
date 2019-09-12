62b161075f99addff231c7a81de287de18eccd43
OKhoma04 <khomaolga04@gmail.com>
2019-09-12 18:01:52 +0300

    Update hw_7_adv.py

--- hw_7_adv.py
+++ hw_7_adv.py
@@ -1,9 +1,8 @@
 import sys
 import pyodbc
 import json
-import decimal
 
-file_sql = sys.argv[1] #argument from cmd
+file_sql = sys.argv[1] #SQL file name - argument from cmd
 
 #connection to MS SQl Server
 conn = pyodbc.connect('Driver={SQL Server};'
@@ -12,18 +11,19 @@ conn = pyodbc.connect('Driver={SQL Server};'
                       'Trusted_Connection=yes;')
 
 
-#execution of SQL file
+#execution of SQL file, retrieveing and converting data to JSON format
 def exec_sql(file_sql):
-    fd=open(file_sql,'r')
+    fd=open(file_sql,'r')       #read SQL file
     sql_file = fd.read()
     fd.close()
     cursor = conn.cursor()
-    cursor.execute(sql_file)
+    cursor.execute(sql_file)    #execute scripts from SQL file(drop table if exist, create table and insert data)
     conn.commit()
+    #retrieve data from created table(MS SQL Server) and convert to JSON format
     cursor.execute('select user_id, username, first_name, last_name, gender, status from dbo.[user_details] FOR JSON AUTO')
-    for row in cursor:
-        print(row)
-
+    results = cursor.fetchall()
+    for row in results:         #print JSON in cmd
+        print (row[0])
 
 exec_sql(file_sql)
 
